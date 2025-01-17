629bb7822f4cf86d6fbaa3052a24dff2bac2ebc2
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-01-17 23:04:35 +0530

    17/01/2025
    .

--- App/screens/BottomScreens/ServiceAdd.js
+++ App/screens/BottomScreens/ServiceAdd.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import moment from 'moment';
 
 const ServiceAdd = () => {
@@ -12,36 +12,43 @@ const ServiceAdd = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Today</Text>
+      <View
+        style={styles.titleView}
+      >
+        <Text style={styles.title}>Today</Text>
+        <TouchableOpacity>
+          <Image source={require('../../assets/vectors/search.png')} style={styles.search} />
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={dates}
         horizontal
+        style={styles.dateFlat}
         keyExtractor={(item) => item}
+        ListFooterComponent={
+        <TouchableOpacity>
+          <Image source={require('../../assets/vectors/allDate.png')} style={styles.allDate} />
+        </TouchableOpacity>
+        }
         renderItem={({ item }) => (
           <TouchableOpacity
-            style={[
-              styles.dateItem,
-              selectedDate === item && styles.selectedDateItem,
-            ]}
+            style={styles.dateItem}
             onPress={() => setSelectedDate(item)}
           >
             <Text
               style={[
                 styles.dayText,
                 moment(item).format('ddd')=== "Sun" && styles.sunday,
-                selectedDate === item && styles.selectedDateText,
               ]}
             >
               {moment(item).format('ddd').toUpperCase()}
             </Text>
             <Text
-              style={[
-                styles.dateText,
-                selectedDate === item && styles.selectedDateText,
-              ]}
+              style={styles.dateText}
             >
               {moment(item).format('DD')}
             </Text>
+            <View  style={[selectedDate === item && styles.selectedDateLine]}/>
           </TouchableOpacity>
         )}
       />
@@ -51,21 +58,42 @@ const ServiceAdd = () => {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#EBEEFF',
+    alignItems: 'center',
+  },
+  titleView: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: 325,
+    marginTop: 20,
   },
   title: {
-    fontSize: 18,
-    marginBottom: 10,
+    fontSize: 24,
+    fontFamily: 'Poppins',
+    color: '#4A4E69',
+    fontWeight: '400',
+  },
+  search: {
+    width: 30,
+    height: 30,
+  },
+  dateFlat: {
+    width: 326,
+    height: 55,
+    alignContent: 'center',
   },
   dateItem: {
+    width: 32,
+    marginRight: 10,
     alignItems: 'center',
-    padding: 8,
+    alignSelf: 'center',
     borderRadius: 5,
-    
   },
-  selectedDateItem: {
-    backgroundColor: '#6200ee',
+  selectedDateLine: {
+    width: 20,
+    height: 5,
+    backgroundColor: '#22223B',
+    borderRadius: 8,
   },
   dateText: {
     fontSize: 20,
@@ -81,8 +109,10 @@ const styles = StyleSheet.create({
   sunday: {
     color: '#E73D3D',
   },
-  selectedDateText: {
-    color: '#fff',
+  allDate: {
+    width: 32,
+    height: 55,
+    resizeMode: 'contain',
   },
 });
 
