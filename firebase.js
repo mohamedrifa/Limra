895e875f667496901f7358fd6080d62e3c56ac0c
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-03-09 19:14:10 +0530

    myFirebase
    .

--- firebase.js
+++ firebase.js
@@ -4,18 +4,24 @@ import { getDatabase } from 'firebase/database';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUJu5RmXrcsg0ZUzoGhey3tWimzk8D_ls",
-  authDomain: "limra-560bd.firebaseapp.com",
-  databaseURL: "https://limra-560bd-default-rtdb.firebaseio.com",
-  projectId: "limra-560bd",
-  storageBucket: "limra-560bd.firebasestorage.app",
-  messagingSenderId: "291840741923",
-  appId: "1:291840741923:web:793fdb4de36ba81284d796",
-  measurementId: "G-H4HDGHE8ND"
+
+  apiKey: "AIzaSyD3SY9AjbBnXjlyjL51vwCosQ-1NqWJ--M",
+
+  authDomain: "limra-7ba51.firebaseapp.com",
+
+  databaseURL: "https://limra-7ba51-default-rtdb.firebaseio.com",
+
+  projectId: "limra-7ba51",
+
+  storageBucket: "limra-7ba51.firebasestorage.app",
+
+  messagingSenderId: "422137489540",
+
+  appId: "1:422137489540:web:66eff63a7b89cba357fad1",
+
+  measurementId: "G-FZJN29Q5PK"
+
 };
-
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
